82a0c53d7d722616e8e26c560be7ec3463c609b6
Devanshu Prakash <devanshuprakash2301@gmail.com>
2026-02-15 01:43:48 +0530

    making main COMPONENT

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { assets } from '../../assets/assets'
+import '../Main/Main.css';
 
 const Main = () => {
   return (
@@ -8,8 +9,30 @@ const Main = () => {
         <p>Forntier-ui </p>
         <img src={assets.user_icon} alt="" />
       </div>
+
       <div className="main-container">
-        <p><span>Hello,user</span></p>
+        <div className="greet">
+          <p><span>Hello, Dev..</span></p>
+          <p>How can i help you today?</p>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>Suggest beautiful places to see in France</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Briefly explain the topic : Learning rate in ml</p>
+            <img src={assets.bulb_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Brainstorm team bonding activities for our work retreat</p>
+            <img src={assets.message_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Improve the readability of my code</p>
+            <img src={assets.code_icon} alt="" />
+          </div>
+        </div>
       </div>
     </div>
   )
